914240ec14c214a179c327c167fe21567e54f334
nicolas jesus alarcon ferrus <nicolasalarcon40@gmail.com>
2026-05-16 13:12:09 -0400

    Creacion modelo del microservicio

---
+++ ms-calificaciones/src/main/java/com/colegio/mscalificaciones/model/Calificacion.java
@@ -0,0 +1,22 @@
+package com.colegio.mscalificaciones.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "calificaciones")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Calificacion {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Double nota;
+    private LocalDate fecha;
+    private String rutEstudiante;
+    private Long idAsignatura;
+}
