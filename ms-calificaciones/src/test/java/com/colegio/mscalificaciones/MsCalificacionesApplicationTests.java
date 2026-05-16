0642a0b8a3c887ea53305bd73e5813664bd2e8c0
nicolas jesus alarcon ferrus <nicolasalarcon40@gmail.com>
2026-05-16 12:05:20 -0400

    Creacion carpeta con nuevo microservicio 'ms calificaciones'

---
+++ ms-calificaciones/src/test/java/com/colegio/mscalificaciones/MsCalificacionesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.colegio.mscalificaciones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsCalificacionesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
