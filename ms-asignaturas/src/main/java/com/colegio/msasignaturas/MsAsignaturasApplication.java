2b074dadf2d810fec95a5a1979aa7de28f89d4ca
nicolas jesus alarcon ferrus <nicolasalarcon40@gmail.com>
2026-05-16 11:24:49 -0400

    Estructura inicial del proyecto ms-asignaturas

---
+++ ms-asignaturas/src/main/java/com/colegio/msasignaturas/MsAsignaturasApplication.java
@@ -0,0 +1,13 @@
+package com.colegio.msasignaturas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsAsignaturasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsAsignaturasApplication.class, args);
+	}
+
+}
---
+++ ms-asignaturas/src/test/java/com/colegio/msasignaturas/MsAsignaturasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.colegio.msasignaturas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsAsignaturasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
